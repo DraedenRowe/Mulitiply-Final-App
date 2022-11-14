cf14a903bed299bb9ff9c40db4f93c3b5869e152
Draeden Rowe <drowe195@d127.org>
2022-11-14 09:38:48 -0600

    Outline

--- Mulitiply Final App/ViewController.swift
+++ Mulitiply Final App/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var number1TextField: UITextField!
+    @IBOutlet weak var number2TextField: UITextField!
+    @IBOutlet weak var operatorLabel: UILabel!
+    @IBOutlet weak var marioKartImageView: UIImageView!
+    @IBOutlet weak var evenOrOddImageView: UIImageView!
+    @IBOutlet weak var operatorsSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var number1Slider: UISlider!
+    @IBOutlet weak var number2Slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+//Draeden Section
+    @IBAction func changeNumber1(_ sender: Any) {
+    }
+    
+    @IBAction func changeNumber2(_ sender: Any) {
+    }
+    
+    @IBAction func changeOperations(_ sender: Any) {
+    }
+    
+    @IBAction func calculate(_ sender: Any) {
+    }
+    
+    
+    
+//Jack Section
+    @IBAction func clearApp(_ sender: Any) {
+    }
+    
+    
 }
 
